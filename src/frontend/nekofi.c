12d7d8751abcfb8871b2a1d7c3c2c0969615b5be
La Ode Muh. Fadlun Akbar <fadlun.net@gmail.com>
2017-03-30 06:20:34 +0700

    frontend: add license header

--- src/frontend/nekofi.c
+++ src/frontend/nekofi.c
@@ -1,3 +1,20 @@
+/*
+ * Copyright (C) 2017 La Ode Muh. Fadlun Akbar <fadlun.net@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include <gtk/gtk.h>
 
 #include "nekofi.h"
@@ -20,7 +37,7 @@ static void
 neko_fi_activate(GApplication *app)
 {
     NekoFiWindow *win = NULL;
-    
+
     win = neko_fi_window_new(NEKO_FI(app));
     gtk_window_present(GTK_WINDOW(win));
 }
