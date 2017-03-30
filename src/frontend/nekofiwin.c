4c3b02116ce8fa8a1416971203b2c19b6ac6b168
La Ode Muh. Fadlun Akbar <fadlun.net@gmail.com>
2017-03-30 20:29:40 +0700

    frontend: create a listbox for devices

--- src/frontend/nekofiwin.c
+++ src/frontend/nekofiwin.c
@@ -57,4 +57,59 @@ neko_fi_window_new(NekoFi *app)
 void neko_fi_window_scan(NekoFiWindow *win)
 {
     NekoFiWindowPrivate *priv = NULL;
+
+    GtkWidget* button_box = NULL;
+    GtkWidget* devs_info = NULL;
+    GtkWidget* button = NULL;
+    GtkWidget* label = NULL;
+    gchar* devs_desc = NULL;
+    GSList* items = NULL;
+    GSList* iterator = NULL;
+
+    priv = neko_fi_window_get_instance_private (win);
+    items = usb_devices_list();
+
+    for (iterator = items; iterator; iterator = iterator->next) {
+        devs_info = gtk_box_new(GTK_ORIENTATION_HORIZONTAL, 0);
+        gtk_widget_show(devs_info);
+        button_box = gtk_box_new(GTK_ORIENTATION_VERTICAL, 0);
+        gtk_widget_show(button_box);
+
+        devs_desc = g_strdup_printf("<b>%s</b>\nidVendor: %s\nidProduct: %s\n"
+                                    "Manufacturer: %s\nBUSID: %s\n",
+                                    ((UsbDevice*)iterator->data)->product_usb,
+                                    ((UsbDevice*)iterator->data)->idVendor,
+                                    ((UsbDevice*)iterator->data)->idProduct,
+                                    ((UsbDevice*)iterator->data)->manufact,
+                                    ((UsbDevice*)iterator->data)->busid);
+
+        label = gtk_label_new(NULL);
+        gtk_label_set_markup(GTK_LABEL(label), devs_desc);
+        gtk_widget_show(label);
+
+        button = gtk_button_new_with_label("Attach");
+        gtk_widget_show(button);
+        gtk_box_set_center_widget(GTK_BOX(button_box), button);
+
+        /*
+        g_signal_connect(button, "clicked", G_CALLBACK(control_usb_remote),
+                         ((UsbDevice*)iterator->data));
+        */
+
+        gtk_box_pack_start(GTK_BOX(devs_info), label, FALSE, FALSE, 0);
+        gtk_box_pack_end(GTK_BOX(devs_info), button_box, FALSE, FALSE, 0);
+        gtk_list_box_insert(GTK_LIST_BOX(priv->scan_result), devs_info, 0);
+
+        /* debugging purpose */
+        g_print("product: %s\n", ((UsbDevice*)iterator->data)->product_usb);
+
+        g_free(devs_desc);
+    }
+
+    for (iterator = items; iterator; iterator = iterator->next) {
+        finish_dev_usage(((UsbDevice*)iterator->data)->dev);
+    }
+
+    g_free(items->data);
+    g_slist_free(items);
 }
