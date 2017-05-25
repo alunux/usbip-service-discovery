8095d55f5697090685361d127c92c7087d5a795a
La Ode Muh. Fadlun Akbar <fadlun.net@gmail.com>
2017-05-25 23:24:20 +0700

    server: monitor device's event

---
+++ src/server/monitor.c
@@ -0,0 +1,76 @@
+/*
+ * Copyright (C) 2017 La Ode Muh. Fadlun Akbar <fadlun.net@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <libudev.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/time.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+int
+main(void)
+{
+    struct udev* udev;
+    struct udev_device* dev;
+    struct udev_monitor* mon;
+
+    int fd;
+
+    udev = udev_new();
+    if (udev == NULL) {
+        perror("udev");
+        exit(1);
+    }
+
+    mon = udev_monitor_new_from_netlink(udev, "udev");
+    udev_monitor_filter_add_match_subsystem_devtype(mon, "usb", NULL);
+    udev_monitor_enable_receiving(mon);
+    fd = udev_monitor_get_fd(mon);
+
+    while (1) {
+        fd_set fds;
+        struct timeval tv;
+        int ret = 0;
+
+        const char* path;
+
+        FD_ZERO(&fds);
+        FD_SET(fd, &fds);
+        tv.tv_sec = 0;
+        tv.tv_usec = 0;
+
+        ret = select(fd + 1, &fds, NULL, NULL, &tv);
+        if (ret > 0 && FD_ISSET(fd, &fds)) {
+            dev = udev_monitor_receive_device(mon);
+            if (dev) {
+                path = udev_device_get_devnode(dev);
+                if (path != NULL) {
+                    if (system("killall send_data") == 0) {
+                        system("./send_data &");
+                    }
+                }
+                udev_device_unref(dev);
+            }
+        }
+
+        usleep(250 * 1000);
+    }
+
+    return EXIT_SUCCESS;
+}
