d5bbea567c6d089e47c351a7e98bc9b1cf84ed90
La Ode Muh. Fadlun Akbar <fadlun.net@gmail.com>
2017-07-25 01:28:43 +0700

    just format the code

--- src/client/nekofi-client-pkexec.c
+++ src/client/nekofi-client-pkexec.c
@@ -3,7 +3,8 @@
 int
 main(void)
 {
-    char* arguments[] = {"pkexec", "nekofi-client"};
-    g_spawn_sync("/", arguments, g_get_environ(), G_SPAWN_SEARCH_PATH, NULL, NULL, NULL, NULL, NULL, NULL);
+    char* arguments[] = { "pkexec", "nekofi-client" };
+    g_spawn_sync("/", arguments, g_get_environ(), G_SPAWN_SEARCH_PATH, NULL,
+                 NULL, NULL, NULL, NULL, NULL);
     return 0;
 }
